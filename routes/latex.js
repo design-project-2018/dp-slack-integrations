b0cc4125db19eff48fc2302fc1a6acd5fefa9f0c
John Wu <john.wu@mail.mcgill.ca>
2018-01-11 17:43:30 -0500

    Took outdb

--- routes/latex.js
+++ routes/latex.js
@@ -1,27 +1,24 @@
-var db = require('../lib/db')
 /**
  * Routes for latex management
  *
  * @param app
  */
 module.exports = function (app) {
-
-    app.get('/', function (req, res) {
-        res.send('Hello')
-    })
+  app.get('/', function (req, res) {
+    res.send('Hello')
+  })
 
     /**
      * Retrieve all latex documents for the project
      */
-    app.get('/documents', function (req, res) {
-        db.query('SELECT * FROM documents', function (err, data) {
-            if (err) {
-                return console.error('error running query', err)
-            }
-            res.send({
-                'documents': data.rows
-            })
-        })
+  app.get('/documents', function (req, res) {
+    res.send({
+      'documents': [
+        {
+          'name': 'Project Proposal',
+          'url': 'https://www.overleaf.com/13160242vmdqjmxwmcgs#/50592578/'
+        }
+      ]
     })
-
+  })
 }
