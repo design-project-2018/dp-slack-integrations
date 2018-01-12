56b815e2982d5c7a5f3966772b7b982490daccd8
John Wu <john.wu@mail.mcgill.ca>
2018-01-11 23:57:36 -0500

    Cleaning code

--- routes/latex.js
+++ routes/latex.js
@@ -1,4 +1,3 @@
-var request = require('request');
 /**
  * Routes for latex management
  *
@@ -13,27 +12,6 @@ module.exports = function (app) {
      * Retrieve all latex documents for the project
      */
   app.post('/documents', function (req, res) {
-      var options = {
-          url: 'https://hooks.slack.com/services/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',
-          method: 'POST',
-          headers: {
-              'User-Agent':       'Super Agent/0.0.1',
-              'Content-Type':     'application/json'
-          },
-          form: {
-              'attachments': [
-                  {
-                      'title': 'Project Proposal',
-                      'text': 'https://www.overleaf.com/13160242vmdqjmxwmcgs#/50592578/'
-                  }
-              ]
-          }
-      }
-      request(options, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-              console.log(body)
-          }
-      })
       res.send({
           'text': 'All latex documents',
           'attachments': [
