4e7e96e1f3c70ac09277e422d4c4123419b9365c
John Wu <john.wu@mail.mcgill.ca>
2018-01-11 23:50:32 -0500

    Updated api

--- routes/latex.js
+++ routes/latex.js
@@ -1,10 +1,10 @@
+var request = require('request');
 /**
  * Routes for latex management
  *
  * @param app
  */
 module.exports = function (app) {
-
   app.get('/', function (req, res) {
     res.send('Hello')
   })
@@ -13,13 +13,27 @@ module.exports = function (app) {
      * Retrieve all latex documents for the project
      */
   app.post('/documents', function (req, res) {
-    res.send({
-      'documents': [
-        {
-          'name': 'Project Proposal',
-          'url': 'https://www.overleaf.com/13160242vmdqjmxwmcgs#/50592578/'
-        }
-      ]
-    })
+      var options = {
+          url: 'https://hooks.slack.com/services/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',
+          method: 'POST',
+          headers: {
+              'User-Agent':       'Super Agent/0.0.1',
+              'Content-Type':     'application/json'
+          },
+          form: {
+              'attachments': [
+                  {
+                      'title': 'Project Proposal',
+                      'text': 'https://www.overleaf.com/13160242vmdqjmxwmcgs#/50592578/'
+                  }
+              ]
+          }
+      }
+      request(options, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body)
+          }
+      })
+      res.send('Complete')
   })
 }
